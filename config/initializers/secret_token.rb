d9852447ca18a1f86b16cfaddb1f26ca05fb02c3
Aaron Sumner <asumner@mac.com>
2013-03-27 20:47:02 -0500

    initial app generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burroughs::Application.config.secret_token = 'd272f0682625fefb6135c3c42c77021917caead56089fde763c9da54b786170356a4088397021b9622d115f8639c4657be7a8105dc804d346a4ce9f37206838e'
