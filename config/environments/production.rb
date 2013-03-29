6c831ed547bdd15759422853bf6cf61bf989d305
Aaron Sumner <asumner@mac.com>
2013-03-29 12:39:37 -0500

    add http basic auth via rack during development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ Burroughs::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Secure with HTTP Auth Basic during development
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
+    u == ENV['HTTPUSER'] && p == ENV['HTTPPASS']
+  end
 end
