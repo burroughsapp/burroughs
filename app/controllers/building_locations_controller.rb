17cd1c13d19585e9f2301b2e886d6550b3d32478
Aaron Sumner <asumner@mac.com>
2013-04-16 22:55:25 -0500

    allow adding/editing building locations

--- app/controllers/building_locations_controller.rb
+++ app/controllers/building_locations_controller.rb
@@ -1,11 +1,12 @@
 class BuildingLocationsController < ApplicationController
+  before_filter :get_building
+  before_filter :get_building_location, except: [:new, :create]
+
   def new
-    @building = Building.find(params[:building_id])
     @building_location = @building.building_locations.build
   end
 
   def create
-    @building = Building.find(params[:building_id])
     @building_location = @building.building_locations.new(params[:building_location])
     if @building_location.save
       redirect_to @building_location.building, notice: 'Successfully created building/location'
@@ -15,5 +16,30 @@ class BuildingLocationsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @building_location.update_attributes(params[:building_location])
+      redirect_to @building_location.building, notice: 'Successfully updated building/location'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @building_location.destroy
+    redirect_to @building, notice: 'Successfuly deleted building/location'
+  end
+
+  private
+
+  def get_building
+    @building = Building.find(params[:building_id]) if params[:building_id]
+  end
+
+  def get_building_location
+    if params[:id]
+      @building_location = @building.building_locations.find(params[:id])
+    end
   end
 end
