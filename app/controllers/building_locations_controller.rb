5831a674ef209501cf3e9df503c540686c9487a4
Aaron Sumner <asumner@mac.com>
2013-04-24 08:42:16 -0500

    update labels

--- app/controllers/building_locations_controller.rb
+++ app/controllers/building_locations_controller.rb
@@ -9,7 +9,7 @@ class BuildingLocationsController < ApplicationController
   def create
     @building_location = @building.building_locations.new(params[:building_location])
     if @building_location.save
-      redirect_to @building_location.building, notice: 'Successfully created building/location'
+      redirect_to @building_location.building, notice: 'Successfully created building location'
     else
       render :new
     end
@@ -20,7 +20,7 @@ class BuildingLocationsController < ApplicationController
 
   def update
     if @building_location.update_attributes(params[:building_location])
-      redirect_to @building_location.building, notice: 'Successfully updated building/location'
+      redirect_to @building_location.building, notice: 'Successfully updated building location'
     else
       render :edit
     end
@@ -28,7 +28,7 @@ class BuildingLocationsController < ApplicationController
 
   def destroy
     @building_location.destroy
-    redirect_to @building, notice: 'Successfuly deleted building/location'
+    redirect_to @building, notice: 'Successfuly deleted building location'
   end
 
   private
